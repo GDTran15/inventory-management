cb2be3eefa50a761c58f4b4d88c50a953e0b882d
GDTran15 <trangiaiduong1512@gmail.com>
2025-08-14 15:26:18 +1000

    register Controller

--- src/main/java/com/project/inventory_management/controller/UserAccessController.java
+++ src/main/java/com/project/inventory_management/controller/UserAccessController.java
@@ -1,25 +1,33 @@
 package com.project.inventory_management.controller;
 
+import com.project.inventory_management.dto.ApiResponse;
+import com.project.inventory_management.dto.CreateAccountRequestDTO;
+import com.project.inventory_management.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
-public class AuthenticationController {
+public class UserAccessController {
+
+    private final UserService userService;
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping
-    public ResponseEntity<String> createAccount(){
-
-        return new ResponseEntity<>("done", HttpStatus.OK);
+    @PostMapping("register")
+    public ResponseEntity<ApiResponse<String>> createAccount(@Valid @RequestBody CreateAccountRequestDTO createAccountRequestDTO){
+        userService.createUser(createAccountRequestDTO);
+        return ResponseEntity.ok(ApiResponse.success("Register successfully"));
     }
 
 
-    @PostMapping
+    @PostMapping("login")
     public ResponseEntity<String> login(){
-
         return new ResponseEntity<>("done", HttpStatus.OK);
     }
 
