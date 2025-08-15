af6d56dc5d2b5d93e9d04d96a2869a7b706b6755
GDTran15 <trangiaiduong1512@gmail.com>
2025-08-16 00:14:43 +1000

    create login controller, get all account and delete controller

--- src/main/java/com/project/inventory_management/controller/UserAccessController.java
+++ src/main/java/com/project/inventory_management/controller/UserAccessController.java
@@ -2,15 +2,18 @@ package com.project.inventory_management.controller;
 
 import com.project.inventory_management.dto.ApiResponse;
 import com.project.inventory_management.dto.CreateAccountRequestDTO;
+import com.project.inventory_management.dto.LoginDTORequest;
+import com.project.inventory_management.dto.LoginDTOResponse;
+import com.project.inventory_management.model.User;
 import com.project.inventory_management.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,17 +21,37 @@ public class UserAccessController {
 
     private final UserService userService;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping("register")
+   @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("users")
     public ResponseEntity<ApiResponse<String>> createAccount(@Valid @RequestBody CreateAccountRequestDTO createAccountRequestDTO){
+        System.out.println("controller");
         userService.createUser(createAccountRequestDTO);
         return ResponseEntity.ok(ApiResponse.success("Register successfully"));
     }
 
 
     @PostMapping("login")
-    public ResponseEntity<String> login(){
-        return new ResponseEntity<>("done", HttpStatus.OK);
+    public ResponseEntity<ApiResponse<LoginDTOResponse>> login(@Valid @RequestBody LoginDTORequest loginDTORequest){
+        LoginDTOResponse loginDTOResponse = userService.loginUser(loginDTORequest);
+        if (loginDTOResponse == null){
+            return ResponseEntity.ok(ApiResponse.error("Login failed"));
+        }
+        return ResponseEntity.ok(ApiResponse.success(loginDTOResponse));
     }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("users")
+    public ResponseEntity<ApiResponse<List<User>>> getAllAccount(){
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(ApiResponse.success(users));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("users/{userId}")
+    public ResponseEntity<ApiResponse<String>> deleteAccount(@PathVariable Long userId){
+       userService.deleteUser(userId);
+       return ResponseEntity.ok(ApiResponse.success("Account was deleted"));
+    }
+
 
 }
