6eede1cf7ccfa12a3fdb870ca9de0393f1b661cc
GDTran15 <trangiaiduong1512@gmail.com>
2025-08-16 12:11:06 +1000

    Create Product repo

---
+++ src/main/java/com/project/inventory_management/repository/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.project.inventory_management.repository;
+
+
+import com.project.inventory_management.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+}
